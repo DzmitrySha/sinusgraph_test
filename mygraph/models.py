d7bd5d4865ee3a4f2f7a26dee10550e15654b360
d.shapovalov <d.shapovalov@vitebsk.energo.net>
2023-03-22 15:53:27 +0300

    добавлены модели

--- mygraph/models.py
+++ mygraph/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# cookbook/ingredients/models.py
+
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name="books", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
