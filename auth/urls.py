7029bbf49f068e56007aef3cffcbf581eadfca85
Dzmitry Shapavalau <ravenbydev@gmail.com>
2023-03-26 17:45:26 +0300

    добавлены роутер для категорий

--- auth/urls.py
+++ auth/urls.py
@@ -6,6 +6,7 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'books', views.BookViewSet)
+router.register(r'categories', views.CategoryViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
