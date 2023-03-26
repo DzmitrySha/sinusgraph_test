255f750b2b31e0c4500d08bff4be47bd9a6165c6
Dzmitry Shapavalau <ravenbydev@gmail.com>
2023-03-26 17:45:09 +0300

    добавлены пути для аутентификации по JWT (rest_framework_simplejwt)

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,15 @@ from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("auth.urls")),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
