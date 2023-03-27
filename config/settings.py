3ceef12543b8a818d57968bfd9fb601811e65f87
d.shapovalov <d.shapovalov@vitebsk.energo.net>
2023-03-27 16:01:49 +0300

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -183,7 +183,7 @@ REST_FRAMEWORK = {
     # ],
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
