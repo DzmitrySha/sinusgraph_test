12f1ce00d208282006dd31f5a0f13bc54f40f951
d.shapovalov <d.shapovalov@vitebsk.energo.net>
2023-03-27 14:09:54 +0300

    permissions off (for BookViewSet)

--- auth/views.py
+++ auth/views.py
@@ -28,7 +28,7 @@ from auth.serializers import (
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = (permissions.IsAuthenticated, )
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
