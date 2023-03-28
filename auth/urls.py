4a0731cf877462730155741af1c65d712b24af45
d.shapovalov <d.shapovalov@vitebsk.energo.net>
2023-03-28 15:41:51 +0300

    remove User and Groups serializers and views

--- auth/urls.py
+++ auth/urls.py
@@ -3,8 +3,6 @@ from rest_framework import routers
 from auth import views
 
 router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
 router.register(r'books', views.BookViewSet)
 router.register(r'categories', views.CategoryViewSet)
 
--- auth/views.py
+++ auth/views.py
@@ -1,28 +1,8 @@
 from rest_framework import viewsets, permissions
-# from django.contrib.auth.models import User, Group
 from mygraph.models import Book, Category
 from auth.serializers import (
-    # UserSerializer, GroupSerializer,
     BookSerializer, CategorySerializer
 )
-
-
-# class UserViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = User.objects.all().order_by('-date_joined')
-#     serializer_class = UserSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-#
-#
-# class GroupViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows groups to be viewed or edited.
-#     """
-#     queryset = Group.objects.all()
-#     serializer_class = GroupSerializer
-#     permission_classes = [permissions.IsAuthenticated]
 
 
 class BookViewSet(viewsets.ModelViewSet):
